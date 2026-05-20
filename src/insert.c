8c285c4b94bed3365e168c3164a02247e887e21c
Ouassim Behlil <bahlil2001@gmail.com>
2026-05-20 16:10:01 +0100

    handle edge cases and fix bugs

--- src/insert.c
+++ src/insert.c
@@ -3,7 +3,7 @@
 
 static void	insert_at_end(t_entry *entry, t_entry *head)
 {
-	t_entry		curr;
+	t_entry		*curr;
 
 	curr = head;
 	while (curr->next)
@@ -19,20 +19,25 @@ int	insert(const char *key, const char *value, t_hashmap *map)
 	unsigned int	hash;
 	t_entry			*entry;
 
+	if (!key || !value || !map || map->size >= map->capacity)
+	{
+		return (0);
+	}
 	hash = fnv1a_32(key);
 	entry = create_entry(key, value, hash);
 	if (!entry)
 	{
 		return (0);
 	}
-	index = hash % map->size;
-	if (!map[index])
+	index = hash % map->capacity;
+	if (!map->buckets[index])
 	{
-		map[index] = entry;
+		map->buckets[index] = entry;
 	}
 	else
 	{
-		insert_at_end(entry, map[index]);
+		insert_at_end(entry, map->buckets[index]);
 	}
+	map->size ++;
 	return (1);
 }
