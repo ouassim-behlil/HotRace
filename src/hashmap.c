e1a30c88e6268701dc676d722e570581775b1aa5
Ouassim Behlil <bahlil2001@gmail.com>
2026-05-20 16:09:13 +0100

    initialize capacity in creation and free buckets on free

--- src/hashmap.c
+++ src/hashmap.c
@@ -9,14 +9,23 @@ t_hashmap	*create_map(void)
 	{
 		return (NULL);
 	}
-	memset(map, 0, sizeof(t_hashmap));
+	ft_memset(map, 0, sizeof(t_hashmap));
+	map->capacity = 1000003;
 	return (map);
 }
 
 void	delete_map(t_hashmap *map)
 {
-	if (map)
+	int		i;
+	if (!map)
 	{
-		free(map);
+		return;
 	}
+	i = 0;
+	while (i < map->capacity)
+	{
+		delete_entry(map->buckets[i]);
+		i ++;
+	}
+	free(map);
 }
